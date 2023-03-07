a734d0e12c55c003a1d91212dd05437a9d375ade
Alexsey Fadeev <fadeevlesha@list.ru>
2023-03-07 21:46:17 +0300

    add CustomApiListener

--- src/test/java/helpers/Browserstack.java
+++ src/test/java/helpers/Browserstack.java
@@ -1,24 +1,24 @@
 package helpers;
 
-import config.BrowserStackConfig;
-import org.aeonbits.owner.ConfigFactory;
 
+import static helpers.CustomApiListener.withCustomTemplates;
 import static io.restassured.RestAssured.given;
 import static java.lang.String.format;
 
 public class Browserstack {
     public static String getVideoUrl(String sessionId) {
-        BrowserStackConfig config = ConfigFactory.create(BrowserStackConfig.class, System.getProperties());
         String url = format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
 
         return given()
                 .log().all()
-                .auth().basic(config.userName(), config.passWord())
+                .filter(withCustomTemplates())
+                .auth().basic("fadeevlesha_9BzJIf", "3EiyZgxpsFj5KN3TRWZy")
                 .when()
                 .get(url)
                 .then()
                 .log().all()
                 .statusCode(200)
-                .extract().path("automation_session.video_url");
+                .extract()
+                .path("automation_session.video_url");
     }
 }
