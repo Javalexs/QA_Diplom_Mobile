078f7fdb327d852e51ae68fb130b4e345527cacf
Alexsey Fadeev <fadeevlesha@list.ru>
2023-03-07 21:54:43 +0300

    add localdriver

--- src/test/java/drivers/LocalMobileDriver.java
+++ src/test/java/drivers/LocalMobileDriver.java
@@ -4,12 +4,10 @@ import com.codeborne.selenide.WebDriverProvider;
 import config.LocalConfig;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
-import lombok.SneakyThrows;
 import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 
-import javax.annotation.ParametersAreNonnullByDefault;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,9 +18,9 @@ import static io.appium.java_client.remote.AutomationName.ANDROID_UIAUTOMATOR2;
 import static io.appium.java_client.remote.MobileBrowserType.ANDROID;
 import static org.apache.commons.io.FileUtils.copyInputStreamToFile;
 
-@ParametersAreNonnullByDefault
+
 public class LocalMobileDriver implements WebDriverProvider {
-    static LocalConfig config = ConfigFactory.create(LocalConfig.class, System.getProperties());
+    static LocalConfig emulatorConfig = ConfigFactory.create(LocalConfig.class, System.getProperties());
 
     public static URL getAppiumServerUrl() {
         try {
@@ -32,28 +30,23 @@ public class LocalMobileDriver implements WebDriverProvider {
         }
     }
 
-    @SneakyThrows
     @Override
     public WebDriver createDriver(Capabilities capabilities) {
-
         UiAutomator2Options options = new UiAutomator2Options();
         options.merge(capabilities);
 
         options.setAutomationName(ANDROID_UIAUTOMATOR2)
-                .setPlatformName("Android")
-//                .setDeviceName("Pixel 4 API 30")
-//                .setPlatformVersion("11.0")
-                .setDeviceName("RFCR90ZMNQP")
-                .setPlatformVersion("13.0")
+                .setPlatformName(ANDROID)
+                .setDeviceName(emulatorConfig.device())
+                .setPlatformVersion(emulatorConfig.osVersion())
                 .setApp(getAppPath())
-                .setAppPackage("org.wikipedia.alpha")
-                .setAppActivity("org.wikipedia.main.MainActivity");
+                .setAppPackage(emulatorConfig.getAppPackage())
+                .setAppActivity(emulatorConfig.getAppActivity());
 
         return new AndroidDriver(getAppiumServerUrl(), options);
     }
 
     private String getAppPath() {
-
         String appUrl = "https://github.com/wikimedia/apps-android-wikipedia/" +
                 "releases/download/latest/app-alpha-universal-release.apk";
         String appPath = "src/test/resources/apps/app-alpha-universal-release.apk";
@@ -68,5 +61,4 @@ public class LocalMobileDriver implements WebDriverProvider {
         }
         return app.getAbsolutePath();
     }
-
 }
