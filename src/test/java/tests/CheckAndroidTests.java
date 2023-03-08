380a1cfa763b7b08a51357472c8cb8f0562e28ae
Alexsey Fadeev <fadeevlesha@list.ru>
2023-03-08 23:42:47 +0300

    add addmetod

--- src/test/java/tests/CheckAndroidTests.java
+++ src/test/java/tests/CheckAndroidTests.java
@@ -3,10 +3,13 @@ package tests;
 import com.codeborne.selenide.CollectionCondition;
 import io.appium.java_client.AppiumBy;
 import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selenide.*;
 import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
@@ -25,6 +28,16 @@ public class CheckAndroidTests extends tests.android.selenide.TestBase {
         step("Verify content found", () -> {
             $$(id("org.wikipedia.alpha:id/page_list_item_title"))
                     .shouldHave(sizeGreaterThan(0));
+        });
+    }
+    @Test
+    @DisplayName("Проверка текста в окне регистрации пользователя на устройстве Android")
+    void checkTextLogInToWikipedia() {
+        step("Жмем кнопку регистрации пользователя", () -> {
+            $(byClassName("android.support.v7.widget.LinearLayoutCompat")).click();
+        });
+        step("Проверяем наличие текста ", () -> {
+            $(id("org.wikipedia.alpha:id/explore_overflow_account_name")).shouldHave(text("Log in to Wikipedia"));
         });
     }
 //
