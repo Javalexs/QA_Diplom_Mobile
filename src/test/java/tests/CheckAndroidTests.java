1afb248c0aa74d54b8a318fe922bb9f1dfb03f1b
Alexsey Fadeev <fadeevlesha@list.ru>
2023-03-08 09:48:35 +0300

    add metod drugoy

--- src/test/java/tests/CheckAndroidTests.java
+++ src/test/java/tests/CheckAndroidTests.java
@@ -12,6 +12,7 @@ import static com.codeborne.selenide.Selenide.*;
 import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
 public class CheckAndroidTests extends tests.android.selenide.TestBase {
+    @Test
     void checkTextLogInToWikipedia() {
         step("Жмем кнопку регистрации пользователя", () -> {
             $(byClassName("android.support.v7.widget.LinearLayoutCompat")).click();
